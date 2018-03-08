cd872ff17577dd465401c4090624c3e225e2f7f8
Charles Seymour <charlesseymour@yahoo.com>
2018-03-08 17:07:04 -0600

    Changed MEDIA_URL

--- team_builder/team_builder/settings.py
+++ team_builder/team_builder/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
-MEDIA_URL = '/avatars/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = "accounts.User"
