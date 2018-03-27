aeb0f775cd420baa08231a347621d72dc46c51cb
Charles Seymour <charlesseymour@yahoo.com>
2018-03-26 22:54:00 -0500

    Added django-formset-js

--- team_builder/projects/forms.py
+++ team_builder/projects/forms.py
@@ -2,11 +2,21 @@ from django import forms
 from django.forms import inlineformset_factory
 from projects import models
 from accounts import models as accounts_models
+from djangoformsetjs.utils import formset_media_js
 
+'''class PositionForm(forms.ModelForm):
+    class Meta:
+        model = models.Position
+        fields = ['title', 'description', 'skills', 'project']
+        
+    class Media(object):
+        js = formset_media_js'''
+        
 
 PositionInlineFormSet =  inlineformset_factory(
     models.Project,
     models.Position,
+    # form=PositionForm,
     fields=('title', 'description', 'skills', 'project'),
     extra=0,
     widgets={'title': forms.TextInput(attrs={'label': '',
